8e7adbad82ebc7f2bafb733dec3a1e9c3f8baf41
DianaAvilesCh <diana.aviles2017@uteq.edu.ec>
2021-11-30 13:25:23 -0500

    Enviar mensaje

---
+++ app/src/main/java/com/example/myapp1/mainEnviar.java
@@ -0,0 +1,24 @@
+package com.example.myapp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class mainEnviar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_enviar);
+
+        //Localizar los controles
+        TextView txtSaludo = (TextView)findViewById(R.id.lblSaludo);
+        //Recuperamos la información pasada en el intent
+        Bundle bundle = this.getIntent().getExtras();
+
+        //Construimos el mensaje a mostrar
+        txtSaludo.setText("Hola!, Bienvenido \n " + bundle.getString("NOMBRE"));
+
+    }
+}
